0e2c331d5c86804332de6408649890cbec0c7daf
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-04-27 17:36:57 -0500

    add window resize shortcuts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,33 +1,34 @@
 -- This is a configuration for Hammerspoon:
--- Modification Keys: Cmd + Ctrl (⌃ + ⌥)
+-- Modification Keys: Cmd + Ctrl + Alt + Shift (⌘ + ⌃ + ⌥ + ⇧)
 -- 1. Currently support window layout management:
---   -  ⌃ ⌥ + [, Toggle current window to left/restore;
---   -  ⌃ ⌥ + ], Toggle current window to right/restore;
---   -  ⌃ ⌥ + M, Toggle current window to maximize/restore;
+--   -  ⌘ + ⌃ + ⌥ + ⇧ + [, Toggle current window to left/restore;
+--   -  ⌘ + ⌃ + ⌥ + ⇧ + ], Toggle current window to right/restore;
+--   -  ⌘ + ⌃ + ⌥ + ⇧ + M, Toggle current window to maximize/restore;
 
 -- 1. --
-hs.window.animationDuration = 0
-previousFrameSizes = {}
-modificationKeys = {"ctrl", "alt"}
+hs.alert("config reloaded")
 
-hs.hotkey.bind(modificationKeys, "T", function()
-	local curWin = hs.window.focusedWindow()
-	hs.alert.show(test[curWin.__tostring])
-end)
+hs.window.animationDuration = 0.01
+previous_frame_sizes = {}
+modification_keys = {"cmd", "ctrl", "alt", "shift"}
 
-hs.hotkey.bind(modificationKeys, "R", function()
+hs.hotkey.bind(modification_keys, "R", function()
+	--hs.notify.new({title="Hammerspoon", informativeText="Config Reloaded"}):send()
 	hs.reload()
-	hs.notify.new({title="Hammerspoon", informativeText="Config Reloaded"}):send()
 end)
 
-function isAlmostEqualToCurWinFrame(geo)
+hs.hotkey.bind(modification_keys, "W", function()
+	hs.alert.show("Hello world!")
+end)
+
+function isAlmostEqualToc_win_frame(geo)
 	local epsilon = 5
-	local curWin = hs.window.focusedWindow()
-	local curWinFrame = curWin:frame()
-	if  math.abs(curWinFrame.x - geo.x) < epsilon and
-		math.abs(curWinFrame.y - geo.y) < epsilon and
-		math.abs(curWinFrame.w - geo.w) < epsilon and
-		math.abs(curWinFrame.h - geo.h) < epsilon then
+	local c_win = hs.window.focusedWindow()
+	local c_win_frame = c_win:frame()
+	if  math.abs(c_win_frame.x - geo.x) < epsilon and
+		math.abs(c_win_frame.y - geo.y) < epsilon and
+		math.abs(c_win_frame.w - geo.w) < epsilon and
+		math.abs(c_win_frame.h - geo.h) < epsilon then
 		return true
 	else
 		return false
@@ -35,36 +36,36 @@ function isAlmostEqualToCurWinFrame(geo)
 end
 
 function getMaxWinFrame()
-	local curWin = hs.window.focusedWindow()
-	return curWin:screen():frame()
+	local c_win = hs.window.focusedWindow()
+	return c_win:screen():frame()
 end
 
 function getFillLeftWinFrame()
-	local curWin = hs.window.focusedWindow()
-	local curWinFrame = curWin:frame()
-	local maxFrame = curWin:screen():frame()
-	curWinFrame.x = maxFrame.x
-	curWinFrame.y = maxFrame.y
-	curWinFrame.w = maxFrame.w / 2
-	curWinFrame.h = maxFrame.h
-	return curWinFrame
+	local c_win = hs.window.focusedWindow()
+	local c_win_frame = c_win:frame()
+	local max_frame = c_win:screen():frame()
+	c_win_frame.x = max_frame.x
+	c_win_frame.y = max_frame.y
+	c_win_frame.w = max_frame.w / 2
+	c_win_frame.h = max_frame.h
+	return c_win_frame
 end
 
 function getFillRightWinFrame()
-	local curWin = hs.window.focusedWindow()
-	local curWinFrame = curWin:frame()
-	local maxFrame = curWin:screen():frame()
-	curWinFrame.x = maxFrame.x + maxFrame.w / 2
-	curWinFrame.y = maxFrame.y
-	curWinFrame.w = maxFrame.w / 2
-	curWinFrame.h = maxFrame.h
-	return curWinFrame
+	local c_win = hs.window.focusedWindow()
+	local c_win_frame = c_win:frame()
+	local max_frame = c_win:screen():frame()
+	c_win_frame.x = max_frame.x + max_frame.w / 2
+	c_win_frame.y = max_frame.y
+	c_win_frame.w = max_frame.w / 2
+	c_win_frame.h = max_frame.h
+	return c_win_frame
 end
 
 function isPredefinedWinFrameSize()
-	if isAlmostEqualToCurWinFrame(getMaxWinFrame()) or
-		isAlmostEqualToCurWinFrame(getFillLeftWinFrame()) or
-		isAlmostEqualToCurWinFrame(getFillRightWinFrame()) then
+	if isAlmostEqualToc_win_frame(getMaxWinFrame()) or
+		isAlmostEqualToc_win_frame(getFillLeftWinFrame()) or
+		isAlmostEqualToc_win_frame(getFillRightWinFrame()) then
 		return true
 	else
 		return false
@@ -72,19 +73,19 @@ function isPredefinedWinFrameSize()
 end
 
 function bindResizeAndRestoreToKeys(key, resize_frame_fn)
-	hs.hotkey.bind(modificationKeys, key, function()
-		local curWin = hs.window.focusedWindow()
-		local curWinFrame = curWin:frame()
+	hs.hotkey.bind(modification_keys, key, function()
+		local c_win = hs.window.focusedWindow()
+		local c_win_frame = c_win:frame()
 		local targetFrame = resize_frame_fn()
 
-		if isPredefinedWinFrameSize() and not isAlmostEqualToCurWinFrame(targetFrame) then
-			curWin:setFrame(targetFrame)
-		elseif previousFrameSizes[curWin:id()] then
-			curWin:setFrame(previousFrameSizes[curWin:id()])
-			previousFrameSizes[curWin:id()] = nil
+		if isPredefinedWinFrameSize() and not isAlmostEqualToc_win_frame(targetFrame) then
+			c_win:setFrame(targetFrame)
+		elseif previous_frame_sizes[c_win:id()] then
+			c_win:setFrame(previous_frame_sizes[c_win:id()])
+			previous_frame_sizes[c_win:id()] = nil
 		else
-			previousFrameSizes[curWin:id()] = curWinFrame
-			curWin:setFrame(targetFrame)
+			previous_frame_sizes[c_win:id()] = c_win_frame
+			c_win:setFrame(targetFrame)
 		end
 	end)
 end
@@ -93,31 +94,97 @@ bindResizeAndRestoreToKeys("M", getMaxWinFrame)
 bindResizeAndRestoreToKeys("[", getFillLeftWinFrame)
 bindResizeAndRestoreToKeys("]", getFillRightWinFrame)
 
+
 -- 2. --
-hs.hotkey.bind(modificationKeys, "H", function()
-	-- move current window left
-	local win = hs.window.focusedWindow()
-	local f   = win:frame()
 
-	f.x = f.x - 10
-	win:setFrame(f)
+function is_right()
+	local c_win        = hs.window.focusedWindow()
+	local c_win_frame  = c_win:frame()
+	local c_win_screen = c_win:screen()
+	local max_frame    = c_win_screen:frame()
+
+	if (c_win_frame.x + c_win_frame.w) == max_frame.w then
+		return true
+	else
+		return false
+	end
+end
+
+function is_bottom()
+	local c_win        = hs.window.focusedWindow()
+	local c_win_frame  = c_win:frame()
+	local c_win_screen = c_win:screen()
+	local max_frame    = c_win_screen:frame()
+
+	--if (c_win_frame.y + c_win_frame.h) == max_frame.h then
+	if math.abs(c_win_frame.y + c_win_frame.h) >= max_frame.h then
+		return true
+	else
+		return false
+	end
+end
+
+hs.hotkey.bind(modification_keys, "l", function()
+	-- increase window width
+	local c_win        = hs.window.focusedWindow()
+	local c_win_frame  = c_win:frame()
+	
+	if is_right() then
+		c_win_frame.x = c_win_frame.x - 50
+		c_win_frame.w = c_win_frame.w + 50
+		c_win:setFrame(c_win_frame)
+	else
+		c_win_frame.w = c_win_frame.w + 50
+		c_win:setFrame(c_win_frame)
+	end
 end)
 
-hs.hotkey.bind(modificationKeys, "K", function()
-	-- move current window right
-	local win = hs.window.focusedWindow()
-	local f   = win:frame()
+hs.hotkey.bind(modification_keys, "j", function()
+	-- decrease window width
+	local c_win = hs.window.focusedWindow()
+	local c_win_frame = c_win:frame()
 
-	f.x = f.x + 10
-	win:setFrame(f)
+	if is_right() then
+		c_win_frame.x = c_win_frame.x + 50
+		c_win_frame.w = c_win_frame.w - 50
+		c_win:setFrame(c_win_frame)
+	else
+		c_win_frame.w = c_win_frame.w - 50
+		c_win:setFrame(c_win_frame)
+	end
 end)
 
--- hs.hotkey.bind(modificationKeys, "J", function()
--- 	local win = hs.window.focusedWindow()
--- 	local f = win:frame()
--- 	local screen = win:screen()
--- 	local max = screen:frame()
 
--- 	f.x = max.x + 10
--- 	win:setFrame(f)
--- end)
+hs.hotkey.bind(modification_keys, "i", function()
+	-- increase window height
+	local c_win = hs.window.focusedWindow()
+	local c_win_frame = c_win:frame()
+
+	if is_bottom() then
+		c_win_frame.y = c_win_frame.y - 50
+		c_win_frame.h = c_win_frame.h + 50
+		c_win:setFrame(c_win_frame)
+	else
+		c_win_frame.h = c_win_frame.h + 50
+		c_win:setFrame(c_win_frame)
+	end
+end)
+
+
+
+hs.hotkey.bind(modification_keys, "k", function()
+	-- decrease window height
+	local c_win        = hs.window.focusedWindow()
+	local c_win_frame  = c_win:frame()
+	local c_win_screen = c_win:screen()
+	local max_frame    = c_win_screen:frame()
+
+	if is_bottom() then
+		c_win_frame.y = c_win_frame.y + 50
+		c_win_frame.h = c_win_frame.h - 50
+		c_win:setFrame(c_win_frame)
+	else
+		c_win_frame.h = c_win_frame.h - 50
+		c_win:setFrame(c_win_frame)
+	end
+end)
