2c07c889cbbaa32609354acf6670589780a46715
livelaughriley <rroach@imodules.com>
2019-05-13 07:49:56 -0500

    add ngen script, powershell profile, ignore desktop.ini

---
+++ .win/Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,51 @@
+Set-Location "C:\Users\RRoach\.dot"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+#(& C:\Users\RRoach\AppData\Local\Continuum\anaconda3\Scripts\conda.exe "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
+function rmd {
+    param($dir)
+    Write-Host "Get-ChildItem $dir -Include * -Recurse | Remove-Item"
+}
+
+function gitstatus {
+    git status
+}
+
+### module imports ###
+Import-Module Get-ChildItemColor
+Import-Module posh-git
+Import-Module oh-my-posh -DisableNameChecking
+Set-Theme Paradox
+
+
+### handy aliases ###
+
+#Set-Alias -name 'trash' -value 'empty-recycle-bin'
+
+Set-Alias -name 'r' -value 'radian'
+
+Set-Alias -name 'gs' -value 'gitstatus'
+
+Set-Alias -name 'l' -value Get-ChildItemColor
+
+Set-Alias -name 'jp' -value jetpack
+
+$dot                   = 'C:\Users\RRoach\.dot'
+$Documents             = $docs = 'C:\Users\RRoach\Documents\'
+$GitHub                = $gh   = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
+$internaldataanalytics = $ida  = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
+$OneDrive              = $oned = $od ='C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
+$Desktop               = $desk = $dt = 'C:\Users\RRoach\Desktop\'
+$Projects              = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
+
+$nvd = 'C:\Users\RRoach\AppData\Local\nvim'
+
