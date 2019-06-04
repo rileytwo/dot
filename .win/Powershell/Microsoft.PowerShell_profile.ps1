32d665159fa3e4555367fa43f079f749279f1dea
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-06-04 15:42:45 -0500

    null

--- .win/Powershell/Microsoft.PowerShell_profile.ps1
+++ .win/Powershell/Microsoft.PowerShell_profile.ps1
@@ -3,20 +3,20 @@ Set-Location "C:\Users\RRoach\.dot"
 winfetch
 
 # Chocolatey profile
-#$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-#if (Test-Path($ChocolateyProfile)) {
-#    Import-Module "$ChocolateyProfile"
-#}
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
 #(& "C:\Users\RRoach\AppData\Local\Continuum\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
 #endregion
 
-function rmd {
-    param($dir)
-    Write-Host "Get-ChildItem $dir -Include * -Recurse | Remove-Item"
-}
+#function rmd {
+#    param($dir)
+#    Write-Host "Get-ChildItem $dir -Include * -Recurse | Remove-Item"
+#}
 
 function git_status {
     git status
@@ -60,13 +60,13 @@ Set-Alias -name 'l' -value Get-ChildItemColor
 
 Set-Alias -name 'which' -value Get-Command
 
-$dot                   = 'C:\Users\RRoach\.dot'
-$Documents             = $docs = 'C:\Users\RRoach\Documents\'
-$GitHub                = $gh   = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
-$internaldataanalytics = $ida  = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
-$OneDrive              = $oned = $od ='C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
-$Desktop               = $desk = $dt = 'C:\Users\RRoach\Desktop\'
-$Projects              = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
+$dot = 'C:\Users\RRoach\.dot'
+$Documents = $docs = 'C:\Users\RRoach\Documents\'
+$GitHub = $gh = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
+$internaldataanalytics = $ida = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
+$OneDrive = $oned = $od = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
+$Desktop = $desk = $dt = 'C:\Users\RRoach\Desktop\'
+$Projects = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
 
 $nvd = 'C:\Users\RRoach\AppData\Local\nvim'
 try { $null = gcm pshazz -ea stop; pshazz init 'default' } catch { }
