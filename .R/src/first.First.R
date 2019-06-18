503b487713585dbedfb0532c58e6fee821acdee8
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-06-18 12:13:21 -0500

    Organize functions

--- .R/src/first.First.R
+++ .R/src/first.First.R
@@ -1,9 +1,15 @@
+
+
 localFiles <- list.files(pattern = "\\.R$", ignore.case = TRUE)
-maxChars <- max(nchar(localFiles))
-numCols <-
-    as.integer((options("width")$width - 2) / (1 + maxChars))  # how many columns will fit?
-fmt <-
-    sprint(" %%-%d", maxChars)                  # left justified in each column
+maxChars   <- max(nchar(localFiles))
+
+# how many columns will fit?
+numCols    <- as.integer((options("width")$width - 2) / (1 + maxChars))
+
+
+# left justified in each column
+fmt <- sprintf(" %%-%d", maxChars)
+
 for (nn in localFiles) {
     if ((match(nn, localFiles) %% numCols) == 1)
         cat(" ")   # indent each row
