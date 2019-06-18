e5a7b91801832fa87e57456edf9390249147bbcf
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-06-18 17:55:48 -0500

    Add OS check

--- .win/Powershell/Microsoft.PowerShell_profile.ps1
+++ .win/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,35 @@
-Set-Location "C:\Users\RRoach\.dot"
-$PSDefaultParameterValues = @{
-    "Format-Table:Autosize" = $True 
-}
-    
+
+if ($IsWindows) {
+    Set-Location "C:\Users\RRoach\.dot"
 #winfetch
 
 # Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
+    $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+    if (Test-Path($ChocolateyProfile)) {
+        Import-Module "$ChocolateyProfile"
+    }
+    $dot = 'C:\Users\RRoach\.dot'
+    $Documents = $docs = 'C:\Users\RRoach\Documents\'
+    $GitHub = $gh = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
+    $internaldataanalytics = $ida = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
+    $OneDrive = $oned = $od = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
+    $Desktop = $desk = $dt = 'C:\Users\RRoach\Desktop\'
+    $Projects = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
+    $nvd = 'C:\Users\RRoach\AppData\Local\nvim'
 }
 
+$PSDefaultParameterValues = @{
+    "Format-Table:Autosize" = $True 
+}
 
 #function git_status_b {
 #    git status -sb
 #}
-
-
 ### module imports ###
 Import-Module Get-ChildItemColor
 Import-Module posh-git
 Import-Module oh-my-posh -DisableNameChecking
 Set-Theme riley
-
-#try { $null = gcm pshazz -ea stop; pshazz init 'default' } catch { }
-#pshazz use kiedtl
 
 ### handy aliases ###
 
@@ -41,20 +46,6 @@ Set-Alias -name 'rr' -value 'radian'
 Set-Alias -name 'r' -value 'Rscript'
 Set-Alias -name 'ttx' -value '~/.dot/usr/ttx.R'
 
-#Set-Alias -name 'trash' -value 'empty-recycle-bin'
-
-
 Set-Alias -name 'l' -value Get-ChildItemColor
-
 Set-Alias -name 'which' -value Get-Command
-
-$dot = 'C:\Users\RRoach\.dot'
-$Documents = $docs = 'C:\Users\RRoach\Documents\'
-$GitHub = $gh = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
-$internaldataanalytics = $ida = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
-$OneDrive = $oned = $od = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
-$Desktop = $desk = $dt = 'C:\Users\RRoach\Desktop\'
-$Projects = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
-
-$nvd = 'C:\Users\RRoach\AppData\Local\nvim'
 
