4875527e8a7c6d3b8ff0a8346ad7ec8b38b9443f
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-06-28 12:38:46 -0500

    tidy up

--- .win/Powershell/Microsoft.PowerShell_profile.ps1
+++ .win/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,51 +1,52 @@
-
 if ($IsWindows) {
     Set-Location "C:\Users\RRoach\.dot"
-#winfetch
 
-# Chocolatey profile
+    # Chocolatey profile
     $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
     if (Test-Path($ChocolateyProfile)) {
         Import-Module "$ChocolateyProfile"
     }
-    $dot = 'C:\Users\RRoach\.dot'
-    $Documents = $docs = 'C:\Users\RRoach\Documents\'
-    $GitHub = $gh = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
-    $internaldataanalytics = $ida = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
-    $OneDrive = $oned = $od = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
-    $Desktop = $desk = $dt = 'C:\Users\RRoach\Desktop\'
-    $Projects = $proj = $pj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
-    $nvd = 'C:\Users\RRoach\AppData\Local\nvim'
+    $dot  = 'C:\Users\RRoach\.dot'
+    $docs = 'C:\Users\RRoach\Documents\'
+    $gh   = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\Github'
+    $ida  = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\internaldataanalytics'
+    $oned = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\'
+    $desk = 'C:\Users\RRoach\Desktop\'
+    $proj = 'C:\Users\RRoach\Documents\OneDrive - IMODULES SOFTWARE\Projects\'
+    $nvd  = 'C:\Users\RRoach\AppData\Local\nvim'
 }
 
 $PSDefaultParameterValues = @{
     "Format-Table:Autosize" = $True 
 }
 
-#function git_status_b {
-#    git status -sb
-#}
-### module imports ###
+# // module imports
 Import-Module Get-ChildItemColor
 Import-Module posh-git
 Import-Module oh-my-posh -DisableNameChecking
 Set-Theme riley
 
-### handy aliases ###
 
-function git-commit { git commit $args }
+## // handy aliases
+# // git
+function git-commit {
+    git commit $args
+}
 Set-Alias -name 'gt' git-commit
 
-function git-remote-update { git remote update $args }
+function git-remote-update {
+    git remote update $args
+}
 Set-Alias -name 'grup' git-remote-update
 
-function git-status { git status $args }
+function git-status {
+    git status $args
+}
 Set-Alias -name 'gs' git-status
 
+# // other
 Set-Alias -name 'rr' -value 'radian'
 Set-Alias -name 'r' -value 'Rscript'
-Set-Alias -name 'ttx' -value '~/.dot/usr/ttx.R'
-
 Set-Alias -name 'l' -value Get-ChildItemColor
 Set-Alias -name 'which' -value Get-Command
 
