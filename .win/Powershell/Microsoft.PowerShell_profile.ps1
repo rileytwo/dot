d0a319b14efc3f7b39dc6b7f7f80baede769e48c
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-07-26 17:06:13 -0500

    fix last commit

--- .win/Powershell/Microsoft.PowerShell_profile.ps1
+++ .win/Powershell/Microsoft.PowerShell_profile.ps1
@@ -10,7 +10,9 @@ if ($IsWindows) {
 
     function Start-Conda {
         (& "C:\Users\RRoach\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-        $Env:CONDA_PROMPT_MODIFIER=""
+    }
+    if (Get-VirtualEnvName) {
+        $Env:CONDA_PROMPT_MODIFIER = ""
     }
 }
 
