8cd6184d4976972926fc0fecceca7990a6f47bd1
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-10-01 18:38:09 -0500

    fix opts

--- .scripts/git-replace.sh
+++ .scripts/git-replace.sh
@@ -8,16 +8,46 @@ display_usage() {
   echo
   echo "Usage: $0"
   echo "  -h, Display this message"
-  echo "  -r <config>"
-  echo "  -n <new>"
+  echo "  -c <config>"
+  echo "  -v <new>"
   echo
   exit 1
 }
+
+raise_error() {
+  local error_message="$*"
+  echo "${error_message}"
+}
+
 
 replace() {
   echo "Replacing ${CONFIG} with ${VALUE}..."
   git config --local "${CONFIG}" "${VALUE}"
 }
 
+
+if [ "$#" == 0 ]; then
+  echo "$#"
+  exit 1
+fi
+
+
+while [[ "$#" -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -c|--config)
+      CONFIG="$2"
+      shift
+      shift
+      ;;
+    -v|--value)
+      VALUE="$2"
+      shift
+      shift
+      ;;
+  esac
+done
+
+replace
 
 
