6c0920f1c8f1ea6847923fead2558c5e540620a4
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-11-29 15:48:14 -0600

    robust-ify script

--- .scripts/bid.sh
+++ .scripts/bid.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-display_usage() {
+function display_usage() {
   echo 
   echo "Usage: $0"
   echo "   -h,  Display this message"
@@ -9,35 +9,48 @@ display_usage() {
   exit 1
 }
 
-raise_error() {
+function raise_error() {
   local error_message="$*"
   echo "${error_message}" 1>&2
 }
 
-get_bundle_identifier() {
+function check_PlistBuddy() {
+  local no_PlistBuddyFound="PlistBuddy was not found"
+  [[ -f /usr/libexec/PlistBuddy ]] || { echo "${no_PlistBuddyFound}"; exit 1; }
+}
+
+function print_bundle_identifier() {
   /usr/libexec/PlistBuddy -c 'Print CFBundleIdentifier' \
-    /Applications/"$1"/Contents/Info.plist
+    "$1"/Contents/Info.plist
   }
 
-while getopts "ha" opt; do
-  case $opt in
-    a)
-      shift
-      get_bundle_identifier "$@"
-      ;;
-    h)
-      display_usage
-      ;;
-    :)
-      echo "-${opt} requires an argument"
-      exit 1
-      ;;
-    *)
-      echo "Invalid option: ${opt}"
-      exit 1
-      ;;
-  esac
-done
 
+function main() {
 
+  check_PlistBuddy
 
+  if [ "$#" -gt 0 ]; then
+    case "$1" in
+      "-a" | "--app" | "--application")
+	shift
+	print_bundle_identifier "$@"
+	;;
+      "-h" | "--help" )
+	display_usage
+	;;
+      :)
+	echo "-$1 requires an argument"
+	exit 1
+	;;
+      *)
+	echo "Invalid option: $1"
+	exit 1
+	;;
+    esac
+  else
+    display_usage
+  fi
+
+}
+
+main "$@"
