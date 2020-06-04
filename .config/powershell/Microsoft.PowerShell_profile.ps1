4a573f7668666dd5efe377a55888dda8d3fb89c5
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-06-04 08:40:12 -0500

    move pwsh aliases to a dedicated aliases file

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -102,21 +102,6 @@ Set-UserTheme -Theme riley
 
 
 ### aliases
-function Get-Path {
-    if ($IsWindows) { $env:PATH.Split(';') } else { $env:PATH.Split(':') }
+if (Test-Path "$((get-item $profile).DirectoryName)/aliases.ps1") {
+    . "$((get-item $profile).DirectoryName)/aliases.ps1"
 }
-
-function Get-ChildItemColorForce { Get-ChildItemColor -Path "$args" -Force }
-
-function Get-GitRepositoryStatus { git status $args }
-function Get-GitConfigGlobal { git config --global --list }
-function Get-GitConfigLocal { git config --local --list }
-
-Set-Alias -Name 'which' -Value Get-Command
-Set-Alias -Name 'path' -Value Get-Path
-Set-Alias -Name 'l' -Value Get-ChildItemColorForce
-Set-Alias -Name 'gngl' Get-GitConfigGlobal
-Set-Alias -Name 'gnll' Get-GitConfigLocal
-Set-Alias -Name 'gs' Get-GitRepositoryStatus
-Set-Alias -Name 'rr' -Value 'radian'
-Set-Alias -Name 'r' -Value 'Rscript'
