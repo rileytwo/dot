4f851c82f56a421c55d7df98250e915bacf66d0b
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-06-22 14:29:40 -0500

    add exa aliases

--- config/powershell/aliases.ps1
+++ config/powershell/aliases.ps1
@@ -1,20 +1,65 @@
 #!/usr/bin/env pwsh
 
-function Get-Path {
-    if ($IsWindows) { $env:PATH.Split(';') } else { $env:PATH.Split(':') }
+if ($IsWindows) {
+    Set-Alias -name 'wslpath' -Value 'Get-WSLPath'
+    function Get-WSLPath {
+        $strlit = '"'
+        wsl wslpath $strlit$args$strlit
+    }
+    
+    Set-Alias -name 'exa' -Value 'Get-ChildItemExa'
+    function Get-ChildItemExa {
+        wsl exa $args
+    }
+    
+    Set-Alias -name 'ee' -Value 'Get-ChildItemExa_lhF'
+    function Get-ChildItemExa_lhF {
+        wsl exa -lhF
+    }
+    
+    Set-Alias -name 'e' -Value 'Get-ChildItemExa_lahF'
+    function Get-ChildItemExa_lahF {
+        wsl exa -lahF
+    }
+    
+    Set-Alias -name 'eg' -Value 'Get-ChildItemExa_lahF__git'
+    function Get-ChildItemExa_lahF__git {
+        wsl exa -lahF --git
+    }
 }
 
-function Get-ChildItemColorForce { Get-ChildItemColor -Path "$args" -Force }
+Set-Alias -Name 'path' -Value Get-Path
+function Get-Path {
+    if ($IsWindows) { 
+        $env:PATH.Split(';') 
+    } else { 
+        $env:PATH.Split(':') 
+    }
+}
 
-function Get-GitRepositoryStatus { git status $args }
-function Get-GitConfigGlobal { git config --global --list }
-function Get-GitConfigLocal { git config --local --list }
+Set-Alias -Name 'l' -Value Get-ChildItemColorForce
+function Get-ChildItemColorForce {
+     Get-ChildItemColor -Path "$args" -Force 
+}
+
+
+Set-Alias -Name 'gs' Get-GitRepositoryStatus
+function Get-GitRepositoryStatus { 
+    git status $args 
+}
+
+
+Set-Alias -Name 'gngl' Get-GitConfigGlobal
+function Get-GitConfigGlobal { 
+    git config --global --list 
+}
+
+
+Set-Alias -Name 'gnll' Get-GitConfigLocal
+function Get-GitConfigLocal { 
+    git config --local --list 
+}
 
 Set-Alias -Name 'which' -Value Get-Command
-Set-Alias -Name 'path' -Value Get-Path
-Set-Alias -Name 'l' -Value Get-ChildItemColorForce
-Set-Alias -Name 'gngl' Get-GitConfigGlobal
-Set-Alias -Name 'gnll' Get-GitConfigLocal
-Set-Alias -Name 'gs' Get-GitRepositoryStatus
 Set-Alias -Name 'rr' -Value 'radian'
 Set-Alias -Name 'r' -Value 'Rscript'
