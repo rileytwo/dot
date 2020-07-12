71a18220a9d02d7ccf5554c59d5c1e4059415865
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-07-12 16:05:32 -0500

    remove unnecessary words

--- config/hammerspoon/conf.lua
+++ config/hammerspoon/conf.lua
@@ -10,34 +10,16 @@ Hammerspoon preferences and configurations
 
 local conf = {}
 
-local console = hs.console
+hs.preferencesDarkMode(true)
 
-conf.global = {}
-conf.global.console = {}
-conf.global.screen = {}
+hs.logger.defaultLogLevel = "error"
 
-conf.global.dark_mode = hs.preferencesDarkMode(true)
+hs.console.darkMode(true)
+hs.console.consoleFont({name="SF Mono", size=12})
+hs.console.outputBackgroundColor{alpha = 0.5}
 
-
-conf.global.console.dark_mode = console.darkMode(true)
-conf.global.console.font      = console.consoleFont({name="SF Mono", size=12})
-conf.global.console.output_bg = console.outputBackgroundColor{
-   alpha = 0.5
-}
-conf.global.console.command_fg = console.consoleCommandColor{
-   red   = 0.25,
-   green = 0.70,
-   blue  = 1.00
-}
-conf.global.console.result_fg = console.consoleResultColor{
-   red   = 0.98,
-   green = 0.95,
-   blue  = 0.64
-}
-conf.global.console.print_fg = console.consolePrintColor{
-   red   = 1.00,
-   green = 1.00,
-   blue  = 1.00
-}
+hs.console.consoleCommandColor{red = 0.25, green = 0.70, blue = 1.00}
+hs.console.consoleResultColor{red = 0.98, green = 0.95, blue = 0.64}
+hs.console.consolePrintColor{red = 1.00, green = 1.00, blue = 1.00}
 
 return conf
