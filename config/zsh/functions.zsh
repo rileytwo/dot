013129c6779784dbd3257bf2626959c7e675a3fa
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-07-22 17:53:15 -0500

    add gl function

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -11,3 +11,13 @@ function preman() {
 function domains() {
   echo -e "${"$(defaults domains)"//, /\\n}"
 }
+
+
+if (( $+aliases[gl] )); then
+  unalias gl     
+fi
+
+function gl() {
+  git log --oneline -n "$@" 2>/dev/null || git log --oneline -n 5
+}
+
