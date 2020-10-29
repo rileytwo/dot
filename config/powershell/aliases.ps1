432eba671c10faf82b59cac7c07de2785abc121d
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-10-29 13:21:57 -0500

    check for wsl

--- config/powershell/aliases.ps1
+++ config/powershell/aliases.ps1
@@ -1,30 +1,42 @@
 #!/usr/bin/env pwsh
 
 if ($IsWindows) {
+    # Maybe add a "Write-WSLError" function ?
+    function Get-WSL() {
+        try {
+            Get-Command wsl -ErrorAction:SilentlyContinue
+        }
+        catch [System.Management.Automation.CommandNotFoundException] {
+            continue
+        }
+    }
+    Get-WSL -ErrorAction:SilentlyContinue
+
     Set-Alias -Name 'wslpath' -Value Get-WSLPath
     function Get-WSLPath($pathWin) {
         if (!$pathWin) {
-            $pathWin = $(Get-Location)
+            [string]$pathWin = $(Get-Location)
         }
-
-        [string]$pathWin = Get-Item -Force -Path ${pathWin}
+        else {
+            [string]$pathWin = $(Get-Item -Force -Path $pathWin)
+        }
 
         $pathWin = $pathWin.Replace('\', '/')
         $pathWin = $(wsl wslpath $pathWin)
 
-        Return $pathWin
+        return $pathWin
     }
-    
+
     Set-Alias -Name 'exa' -Value Get-ChildItemExa
     function Get-ChildItemExa($pathWin) {
         wsl exa $(Get-WSLPath -pathWin $pathWin) "$args"
     }
-    
+
     Set-Alias -Name 'ee' -Value Get-ChildItemExa_lhF
     function Get-ChildItemExa_lhF {
         Get-ChildItemExa -pathWin "$args" -lhF
     }
-    
+
     Set-Alias -Name 'e' -Value Get-ChildItemExa_lahF
     function Get-ChildItemExa_lahF {
         Get-ChildItemExa -pathWin "$args" -lahF
@@ -33,10 +45,10 @@ if ($IsWindows) {
 
 Set-Alias -Name 'path' -Value Get-Path
 function Get-Path {
-    if ($IsWindows) { 
-        $env:PATH.Split(';') 
-    } else { 
-        $env:PATH.Split(':') 
+    if ($IsWindows) {
+        $env:PATH.Split(';')
+    } else {
+        $env:PATH.Split(':')
     }
 }
 
@@ -45,7 +57,7 @@ Set-Alias -Name 'which' -Value Get-Command
 Set-Alias -Name 'rr' -Value 'radian'
 
 
-Set-Alias -Name 'gcd' -Value Set-GitBranch_Dev 
+Set-Alias -Name 'gcd' -Value Set-GitBranch_Dev
 function Set-GitBranch_Dev {
     git checkout dev
 }
@@ -56,24 +68,24 @@ function set-GitBranch_Main {
 }
 
 Set-Alias -Name 'gs' Get-GitRepositoryStatus
-function Get-GitRepositoryStatus { 
-    git status $args 
+function Get-GitRepositoryStatus {
+    git status $args
 }
 
 Set-Alias -Name 'gngl' -Value Get-GitConfigGlobal
-function Get-GitConfigGlobal { 
-    git config --global --list 
+function Get-GitConfigGlobal {
+    git config --global --list
 }
 
 
 Set-Alias -Name 'gnll' -Value Get-GitConfigLocal
-function Get-GitConfigLocal { 
-    git config --local --list 
+function Get-GitConfigLocal {
+    git config --local --list
 }
 
 Set-Alias -Name 'l' -Value Get-ChildItemColorForce
 function Get-ChildItemColorForce {
-    Get-ChildItemColor -Path "$args" -Force 
+    Get-ChildItemColor -Path "$args" -Force
 }
 
 
